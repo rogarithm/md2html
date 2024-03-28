33417c17ccbf3ef4fd60aec708a87df0b6fbe6a7
sehoon.gim <sehoongim@gmail.com>
2024-03-28 15:34:04 +0900

    최상위 모듈에서 마크다운을 html로 변환할 수 있다

--- lib/md2html.rb
+++ lib/md2html.rb
@@ -1,2 +1,12 @@
-class Md2Html
+require_relative 'md2html/tokenizer'
+require_relative 'md2html/parser'
+require_relative 'md2html/generator'
+
+module Md2Html
+  def self.make_html(markdown)
+    tokens = Md2Html::Tokenizer::tokenize(markdown)
+    ast    = Md2Html::Parser::parse(tokens)
+    html   = Md2Html::Generator::generate(ast)
+    return html
+  end
 end
