9c83ba5ee1db43a210c528f42b6e6b3fb5dd586b
sehoon.gim <sehoongim@gmail.com>
2024-04-04 12:26:22 +0900

    더 단순한 테스트를 추가한다

--- test/run_tests.rb
+++ test/run_tests.rb
@@ -1,16 +1,17 @@
-require 'md2html'
+require_relative '../lib/md2html'
 require 'test/unit'
 
 include Test::Unit::Assertions
 
+path = 'bold/bold'
 # 마크다운 포맷 테스트 데이터를 읽어온다
 # 읽어온 데이터에 md2html.make_html 메서드를 적용한다
-md_file = File.read(File.expand_path('test/data/paragraph/one_para.md'))
+md_file = File.read(File.expand_path("test/data/#{path}.md"))
 puts "----markdown file content----"
 puts md_file
 
 # html 포맷 테스트 데이터를 읽어온다
-html_file = File.read(File.expand_path('test/data/paragraph/one_para.html'))
+html_file = File.read(File.expand_path("test/data/#{path}.html"))
 puts "----html file content----"
 puts html_file
 assert_equal(html_file, Md2Html::make_html(md_file))
