9b40347af75e21aaffc3aca14f09b756ddde083d
sehun-kim <rogarithm@gmail.com>
2024-04-10 21:38:42 +0900

    깨지는 테스트를 고친다

--- spec/generator_spec.rb
+++ spec/generator_spec.rb
@@ -12,14 +12,25 @@ describe Md2Html::Generator do
   end
 
   it "generates html from paragraph" do
-    expect(generate("__Foo__ and *text*.\n\nAnother para.")).to eq "<p><strong>Foo</strong> and <em>text</em>.</p><p>Another para.</p>"
+    expect(generate("__Foo__ and *text*.\nAnother para.")).to eq "<p>
+  <strong>Foo</strong> and <em>text</em>.
+</p>
+<p>
+  Another para.
+</p>\n"
   end
 
   it "generates html from 1 list item" do
-    expect(generate("- foo\n")).to eq "<ul><li>foo</li></ul>"
+    expect(generate("- foo\n")).to eq "<ul>
+  <li>foo</li>
+</ul>\n"
   end
 
   it "generates html from list" do
-    expect(generate("- foo\n- bar\n- baz\n")).to eq "<ul><li>foo</li><li>bar</li><li>baz</li></ul>"
+    expect(generate("- foo\n- bar\n- baz\n")).to eq "<ul>
+  <li>foo</li>
+  <li>bar</li>
+  <li>baz</li>
+</ul>\n"
   end
 end
--- spec/md2html_spec.rb
+++ spec/md2html_spec.rb
@@ -3,15 +3,26 @@ require 'pry'
 
 describe Md2Html do
   it "generates html from paragraph" do
-    expect(Md2Html::make_html("__Foo__ and *text*.\n\nAnother para.")).to eq "<p><strong>Foo</strong> and <em>text</em>.</p><p>Another para.</p>"
+    expect(Md2Html::make_html("__Foo__ and *text*.\nAnother para.")).to eq "<p>
+  <strong>Foo</strong> and <em>text</em>.
+</p>
+<p>
+  Another para.
+</p>\n"
   end
 
   it "generates html from 1 list item" do
-    expect(Md2Html::make_html("- foo\n")).to eq "<ul><li>foo</li></ul>"
+    expect(Md2Html::make_html("- foo\n")).to eq "<ul>
+  <li>foo</li>
+</ul>\n"
   end
 
   it "generates html from list" do
-    expect(Md2Html::make_html("- foo\n- bar\n- baz\n")).to eq "<ul><li>foo</li><li>bar</li><li>baz</li></ul>"
+    expect(Md2Html::make_html("- foo\n- bar\n- baz\n")).to eq "<ul>
+  <li>foo</li>
+  <li>bar</li>
+  <li>baz</li>
+</ul>\n"
   end
 end
 
