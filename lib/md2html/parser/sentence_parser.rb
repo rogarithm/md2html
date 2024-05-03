4a28996f875147399e22eb0922fe36b0b8e0613b
sehun-kim <rogarithm@gmail.com>
2024-05-03 21:44:09 +0900

    로그 함수 정리
     - sentence_parser

--- lib/md2html/parser/sentence_parser.rb
+++ lib/md2html/parser/sentence_parser.rb
@@ -1,28 +1,36 @@
-require 'logger'
-
 require_relative "base_parser"
 require_relative "matches_first"
 
+require_relative '../util/logger_factory'
 
 module Md2Html
   module Parser
     class SentenceParser < BaseParser
       include MatchesFirst
 
-      def match(tokens)
-        path = "#{File.dirname(__FILE__).split("/")[-2..-1].join("/")}/#{File.basename(__FILE__)}"
-        log = Logger.new('.md2html.log')
-        log.level = Logger::DEBUG
-        log.datetime_format = "%H:%M:%S"
+      def initialize
+        @logger = Md2Html::Util::LoggerFactory.make_logger()
+      end
 
-        log.debug("#{path} parsing #{tokens.count} tokens...")
+      def match(tokens)
+        make_log_msg_before(@logger, tokens)
         node = match_first(tokens, dash_parser, emphasis_parser, bold_parser, text_parser)
-        if node.null? == false
-          log.debug("#{path} parse success. detail: <@type=\"#{node.type}\", @value=\"#{node.value}\", @consumed=#{node.consumed}>")
-        else
-          log.debug("#{path} parse failed. result is NullNode")
-        end
+        make_log_msg_after(@logger, node)
         node
+      end
+
+      def make_log_msg_before(logger, tokens)
+        path = "#{File.dirname(__FILE__).split("/")[-2..-1].join("/")}/#{File.basename(__FILE__)}"
+        logger.debug("#{path} parsing #{tokens.count} tokens...")
+      end
+
+      def make_log_msg_after(logger, node)
+        path = "#{File.dirname(__FILE__).split("/")[-2..-1].join("/")}/#{File.basename(__FILE__)}"
+        if node.null? == false
+          logger.debug("#{path} parse success. detail: <@type=\"#{node.type}\", @value=\"#{node.value}\", @consumed=#{node.consumed}>")
+        else
+          logger.debug("#{path} parse failed. result is NullNode")
+        end
       end
     end
   end
