2412bacb1734748aaaf32a680822928302cb4a6c
sehun-kim <rogarithm@gmail.com>
2024-05-03 22:02:06 +0900

    로그 함수 정리
     - sentences_and_newline_parser

--- lib/md2html/parser/sentences_and_newline_parser.rb
+++ lib/md2html/parser/sentences_and_newline_parser.rb
@@ -1,21 +1,20 @@
-require 'logger'
-
 require_relative "base_parser"
 require_relative "matches_star"
 require_relative "node"
 require_relative "paragraph_node"
+
+require_relative '../util/logger_factory'
 
 module Md2Html
   module Parser
     class SentencesAndNewlineParser < BaseParser
       include MatchesStar
 
-      def match(tokens)
-        path = "#{File.dirname(__FILE__).split("/")[-2..-1].join("/")}/#{File.basename(__FILE__)}"
-        log = Logger.new('.md2html.log')
-        log.level = Logger::DEBUG
-        log.datetime_format = "%H:%M:%S"
+      def initialize
+        @logger = Md2Html::Util::LoggerFactory.make_logger()
+      end
 
+      def match(tokens)
         nodes, consumed = match_star tokens, with: sentence_parser
         return Node.null if nodes.empty?
         if tokens.peek_at(consumed, 'NEWLINE', 'NEWLINE')
@@ -27,13 +26,17 @@ module Md2Html
         end
 
         pn = ParagraphNode.new(sentences: nodes, consumed: consumed)
-        log.debug("#{path} CONSUMED: #{pn.consumed}")
-        all_sentences = "["
-        pn.sentences.each {|s| all_sentences << "<@type=\"#{s.type}\", @value=\"#{s.value}\", @consumed=#{s.consumed}>, "}
-        all_sentences << "]"
-        log.debug("#{path} all_sentences: #{all_sentences}")
-
+        make_log_msg_after(@logger, pn)
         pn
+      end
+
+      def make_log_msg_after(logger, paragraph_node)
+        path = "#{File.dirname(__FILE__).split("/")[-2..-1].join("/")}/#{File.basename(__FILE__)}"
+        logger.debug("#{path} CONSUMED: #{paragraph_node.consumed}")
+        all_sentences = "["
+        paragraph_node.sentences.each {|s| all_sentences << "<@type=\"#{s.type}\", @value=\"#{s.value}\", @consumed=#{s.consumed}>, "}
+        all_sentences << "]"
+        logger.debug("#{path} all_sentences: #{all_sentences}")
       end
     end
   end
