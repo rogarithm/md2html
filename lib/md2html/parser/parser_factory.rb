d8ac9d946f07a5cab8ea7f7c7c9507df2a6b425b
sehun-kim <rogarithm@gmail.com>
2024-05-19 22:37:09 +0900

    dash, emphasis, bold parser 쓰는 곳을 지운다

--- lib/md2html/parser/parser_factory.rb
+++ lib/md2html/parser/parser_factory.rb
@@ -1,6 +1,4 @@
 require_relative "inline_parser"
-require_relative "bold_parser"
-require_relative "emphasis_parser"
 require_relative "sentence_parser"
 require_relative "text_parser"
 require_relative "block_parser"
@@ -8,7 +6,6 @@ require_relative "paragraph_parser"
 require_relative "sentences_and_eof_parser"
 require_relative "sentences_and_newline_parser"
 require_relative "body_parser"
-require_relative "dash_parser"
 require_relative "list_parser"
 require_relative "list_item_parser"
 require_relative "list_items_and_newline_parser"
@@ -19,8 +16,6 @@ module Md2Html
     class ParserFactory
       PARSERS = {
         inline_parser:                 InlineParser,
-        bold_parser:                   BoldParser,
-        emphasis_parser:               EmphasisParser,
         text_parser:                   TextParser,
         sentence_parser:               SentenceParser,
         block_parser:                  BlockParser,
@@ -28,7 +23,6 @@ module Md2Html
         sentences_and_eof_parser:      SentencesAndEofParser,
         sentences_and_newline_parser:  SentencesAndNewlineParser,
         body_parser:                   BodyParser,
-        dash_parser:                   DashParser,
         list_parser:                   ListParser,
         list_item_parser:              ListItemParser,
         list_items_and_newline_parser: ListItemsAndNewlineParser,
