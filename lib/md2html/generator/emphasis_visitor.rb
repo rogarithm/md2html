cd224a41a3f4bafe7b0b32eeb975122f77c627ff
sehun-kim <rogarithm@gmail.com>
2024-06-08 14:39:00 +0900

    코드 정리한다
     - 단순한 일만 하는 visitor를 지운다

--- lib/md2html/generator/emphasis_visitor.rb
+++
@@ -1,9 +0,0 @@
-module Md2Html
-  module Generator
-    class EmphasisVisitor
-      def visit(node)
-        "<em>#{node.value}</em>"
-      end
-    end
-  end
-end
--- lib/md2html/generator/sentence_visitor.rb
+++ lib/md2html/generator/sentence_visitor.rb
@@ -1,30 +1,24 @@
-require_relative 'bold_visitor'
-require_relative 'emphasis_visitor'
-require_relative 'text_visitor'
-require_relative 'dash_visitor'
-require_relative 'newline_visitor'
-
 module Md2Html
   module Generator
     class SentenceVisitor
       SENTENCE_VISITORS = {
-        "BOLD"     => BoldVisitor,
-        "EMPHASIS" => EmphasisVisitor,
-        "TEXT"     => TextVisitor,
-        "DASH"     => DashVisitor,
-        "NEWLINE"  => NewlineVisitor
+        "BOLD"     => ->(n) {"<strong>#{n.value}</strong>"},
+        "EMPHASIS" => ->(n) {"<em>#{n.value}</em>"},
+        "TEXT"     => ->(n) {"#{n.value}"},
+        "DASH"     => ->(n) {"#{n.value}"},
+        "NEWLINE"  => ->(n) {"<br>"}
       }.freeze
 
       def visit(sentence_node)
         sentence_node.words.map do |word|
-          "#{visitor_for(word).visit(word)}"
+          "#{visitor_for(word).call(word)}"
         end.join
       end
 
       private
 
       def visitor_for(word)
-        SENTENCE_VISITORS.fetch(word.type) { raise "Invalid sentence node type! It might be there's no sufficient visitor for a character or markdown element to generate html. If it's the case, consider add a new visitor for that." }.new
+        SENTENCE_VISITORS[word.type] { raise "Invalid sentence node type! It might be there's no sufficient visitor for a character or markdown element to generate html. If it's the case, consider add a new visitor for that." }
       end
     end
   end
