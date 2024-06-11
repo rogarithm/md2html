be8f5c6b5b134e4effbcfee93c6a5c730fdcc877
sehun-kim <rogarithm@gmail.com>
2024-06-11 22:25:01 +0900

    heading 파서 로직 정리한다

--- lib/md2html/parser/heading_parser.rb
+++ lib/md2html/parser/heading_parser.rb
@@ -6,12 +6,20 @@ module Md2Html
   module Parser
     class HeadingParser < BaseParser
       def match(tokens)
-        return Node.null unless tokens.peek_or(%w(HASH TEXT NEWLINE))
-        if tokens.peek_or(%w(HASH TEXT NEWLINE NEWLINE)) == true
+        return Node.null if tokens.peek_from(0, 'HASH') == false
+
+        left = Node.null
+        hash_count = 0
+        if tokens.peek_from(0, 'HASH')
+          left = tokens.offset(1)
+          hash_count = 1
+        end
+
+        if left.peek_or(%w(TEXT NEWLINE NEWLINE)) == true
           Node.new(type: 'HEADING', value: tokens.second.value, consumed: 4)
-        elsif tokens.peek_or(%w(HASH TEXT NEWLINE EOF)) == true
+        elsif left.peek_or(%w(TEXT NEWLINE EOF)) == true
           Node.new(type: 'HEADING', value: tokens.second.value, consumed: 4)
-        elsif tokens.peek_or(%w(HASH TEXT NEWLINE)) == true
+        elsif left.peek_or(%w(TEXT NEWLINE)) == true
           Node.new(type: 'HEADING', value: tokens.second.value, consumed: 3)
         end
       end
