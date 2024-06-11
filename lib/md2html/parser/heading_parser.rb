a2fa239870ad95c241e0d16ca148e71baec9350a
sehun-kim <rogarithm@gmail.com>
2024-06-11 22:42:20 +0900

    level 2 heading을 파싱할 수 있다

--- lib/md2html/parser/heading_parser.rb
+++ lib/md2html/parser/heading_parser.rb
@@ -10,17 +10,25 @@ module Md2Html
 
         left = Node.null
         hash_count = 0
-        if tokens.peek_from(0, 'HASH')
+        if tokens.peek_from(0, 'HASH', 'HASH')
+          left = tokens.offset(2)
+          hash_count = 2
+        elsif tokens.peek_from(0, 'HASH')
           left = tokens.offset(1)
           hash_count = 1
         end
 
+        node_type = 'HEADING'
+        if hash_count == 2
+          node_type += '_LEVEL2'
+        end
+
         if left.peek_or(%w(TEXT NEWLINE NEWLINE)) == true
-          Node.new(type: 'HEADING', value: tokens.second.value, consumed: 4)
+          Node.new(type: node_type, value: left.first.value, consumed: 3 + hash_count)
         elsif left.peek_or(%w(TEXT NEWLINE EOF)) == true
-          Node.new(type: 'HEADING', value: tokens.second.value, consumed: 4)
+          Node.new(type: node_type, value: left.first.value, consumed: 3 + hash_count)
         elsif left.peek_or(%w(TEXT NEWLINE)) == true
-          Node.new(type: 'HEADING', value: tokens.second.value, consumed: 3)
+          Node.new(type: node_type, value: left.first.value, consumed: 2 + hash_count)
         end
       end
     end
